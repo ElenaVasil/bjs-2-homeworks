f27acef58d6f09a755709672d79030896c9568a6
ElenaVasil <marsusha@yandex.ru>
2022-08-07 18:00:57 +0300

    Solved task1 and task2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,23 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if(arr1.length !== arr2.length) {
+    result = false; 
+  } else {
+      //for (let i=0; i < arr1.length; i++) {
+        //if (arr1[i] === arr2[i]) {
+          if (arr1.every((item, i) => item === arr2[i])) {
+            result = true;  
+        } else result = false;
+      //}
+    }
   return result; // boolean
 }
+
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item*10);
+  
   return resultArr; // array
 }
