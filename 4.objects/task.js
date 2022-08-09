96205d4eeb613c7d9eaaa98ad5bd0878c2bf765f
ElenaVasil <marsusha@yandex.ru>
2022-08-09 19:49:14 +0300

    Solved task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  let marks = [];
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+  } else {
+      this.marks.push(mark);
+    }
+}
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+}
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach((mark) => {
+    sum += mark;
+  })
+  // for (let i=0; i< this.marks.length; i++) {
+  //   sum += this.marks[i];
+  // }
+  return (sum/this.marks.length);
+}
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
+
+
